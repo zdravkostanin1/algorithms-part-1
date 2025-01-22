6d57320cc995bef40b23704e533aae8f614a46cf
zdravkostanin1 <zdravko.stanin@gmail.com>
2025-01-22 18:22:05 +0200

    Starting the new solution to RandomWord

--- module-1/hello/RandomWord.java
+++ module-1/hello/RandomWord.java
@@ -4,8 +4,15 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+
 public class RandomWord {
     public static void main(String[] args) {
+        String champion = "";
+        int index = 0;
 
+        while (!StdIn.isEmpty()) {
+
+        }
     }
 }
