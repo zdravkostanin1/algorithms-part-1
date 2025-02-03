21f794001b522859130219cfcf724728e5d3a387
zdravkostanin1 <zdravko.stanin@gmail.com>
2025-02-03 12:02:50 +0200

    Adding docs to one of my solutions of the interview questions.

--- module-2/interview-questions/src/Main.java
+++ module-2/interview-questions/src/Main.java
@@ -1,10 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/**
+ * Union-find with specific canonical element.
+ * Add a method find() to the union-find data type so that find(i) returns the largest element in the connected
+ * component containing i.
+ * The operations, union(), connected(), and find() should all take logarithmic time or better.
+ * For example, if one of the connected components is {1, 2, 6, 9}, then the find() method should
+ * return 9 for each of the four elements in the connected components.
+ */
 public class Main {
 
     public static void main(String[] args) {
         int[] elements = {1, 2, 6, 9};
-        System.out.println(find(7, elements));
+        System.out.println(find(9, elements));
     }
 
     public static int find(int i, int[] elements) {
